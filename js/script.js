c5a960d9a53bb96bdfc5c2f1aed4b755c4c10d57
paulohenriqueo <paulohenrique300305@gmail.com>
2025-07-15 12:44:04 -0300

    :sparkles: feat: closes #4 - produtos na tabela

---
+++ js/script.js
@@ -0,0 +1,96 @@
+//masks
+$("#inputPrice").mask('000.000.000.000.000,00', {reverse: true});
+
+var products = [
+    {
+        id: 1,
+        name: "Computador M1-TX",
+        description: "Intel I7, 16GB, SSD 256, HD 1T",
+        price: 4900,
+        category: 1,
+        promotion: true,
+        new: true
+    },
+    {
+        id: 2,
+        name: "Computador M2-TX",
+        description: "Intel I7, 32GB, SSD 512, HD 1T",
+        price: 5900,
+        category: 2,
+        promotion: false,
+        new: true
+    },
+    {
+        id: 3,
+        name: "Computador M1-T",
+        description: "Intel I5, 16GB, HD 1T",
+        price: 2900,
+        category: 3,
+        promotion: false,
+        new: false
+    },
+];
+var categories = [
+    { id: 1, name: "Produção Própria" },
+    { id: 2, name: "Nacional" },
+    { id: 3, name: "Importado" }
+];  
+
+//onLoad
+loadProducts();
+
+//load all products
+function loadProducts() {
+    for(let prod of products){
+        addNewRow(prod);
+    }
+}
+
+//add new row
+function addNewRow(prod){
+    var table = document.getElementById("productsTable");
+
+    var newRow = table.insertRow();
+
+    //insert product id
+    var idNode = document.createTextNode(prod.id);
+    newRow.insertCell().appendChild(idNode);
+
+    //insert product name
+    var nameNode = document.createTextNode(prod.name);
+    newRow.insertCell().appendChild(nameNode);
+
+    //insert product description
+    var descriptionNode = document.createTextNode(prod.description);
+    newRow.insertCell().appendChild(descriptionNode);
+
+    //insert product price    
+
+
+    var formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
+
+
+    var priceNode = document.createTextNode(formatter.format(prod.price));
+    newRow.insertCell().appendChild(priceNode);
+
+    //insert product category
+    var categoryNode = document.createTextNode(categories[prod.category - 1].name);
+    newRow.insertCell().appendChild(categoryNode);
+
+    //insert product options
+    var options = '';
+    if(prod.promotion) {
+       options = '<span class="badge text-bg-success me-1">P</span>';
+    }
+    if(prod.new) {
+        options += '<span class="badge text-bg-primary">L</span>';
+    }
+
+    newRow.insertCell().innerHTML = options;
+
+
+}
